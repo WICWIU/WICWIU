4dd051ed70f5c8a4b005584890843970b5b39e14
chuniee0126 <chunie0126@gmail.com>
2018-08-14 22:40:27 +0900

    Update ImageNet Data Loader Test code

--- tutorials/ImageNet/main_test.cpp
+++ tutorials/ImageNet/main_test.cpp
@@ -5,42 +5,31 @@
 int main(int argc, char const *argv[]) {
     std::cout << "======================START=========================" << '\n';
 
-    ImageNetDataReader<float> *data_reader = new ImageNetDataReader<float>(200, 10, TRUE);
+    ImageNetDataReader<float> *data_reader = new ImageNetDataReader<float>(200, 10, FALSE);
 
     Tensor<float> **data = NULL;
 
     sleep(3);
 
-    data = data_reader->GetDataFromBuffer();
+    for(int i = 0; i < 250; i++){
+        std::cout << "i : " << i << '\n';
+        data = data_reader->GetDataFromBuffer();
 
-    std::cout << data[1]->GetShape() << '\n';
-    std::cout << data[0]->GetShape() << '\n';
+        std::cout << data[1]->GetShape() << '\n';
+        std::cout << data[0]->GetShape() << '\n';
 
-    delete data[0];
-    delete data[1];
+        delete data[0];
+        delete data[1];
 
-    delete data;
-
-    sleep(3);
-
-    data = data_reader->GetDataFromBuffer();
-
-    std::cout << data[1]->GetShape() << '\n';
-    std::cout << data[0]->GetShape() << '\n';
-
-    delete data[0];
-    delete data[1];
-
-    delete data;
-
-    sleep(3);
+        delete data;
+    }
 
     data_reader->StopDataPreprocess();
 
     sleep(3);
 
     delete data_reader;
-    
+
     std::cout << "======================Done=========================" << '\n';
 
     return 0;
