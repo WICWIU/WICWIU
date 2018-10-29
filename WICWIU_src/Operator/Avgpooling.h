81eedda17efa5dd05406831cd83c7133c47fa944
chuniee0126 <chunie0126@gmail.com>
2018-10-29 16:07:02 +0900

    Update Densenet for imageNet

--- WICWIU_src/Operator/Avgpooling.h
+++ WICWIU_src/Operator/Avgpooling.h
@@ -67,7 +67,7 @@ public:
 
         checkCUDNN(cudnnCreatePoolingDescriptor(&m_aPoolingDesc));
 
-        checkCUDNN(cudnnSetPooling2dDescriptor(m_aPoolingDesc, CUDNN_POOLING_AVERAGE_COUNT_INCLUDE_PADDING, CUDNN_PROPAGATE_NAN,
+        checkCUDNN(cudnnSetPooling2dDescriptor(m_aPoolingDesc, CUDNN_POOLING_AVERAGE_COUNT_EXCLUDE_PADDING, CUDNN_PROPAGATE_NAN,
                                                m_rowsize, m_colsize,  // mask
                                                0, 0, // padding
                                                m_rowsize, m_colsize // stride
@@ -179,6 +179,174 @@ public:
           // this->BackPropagate(pTime);
           return TRUE;
       }
+
+#endif  // if __CUDNN__
+};
+
+
+
+template<typename DTYPE> class AvaragePooling2D : public Operator<DTYPE>{
+private:
+    int m_stride[2];
+    int m_mask[2];
+    int m_padding[2];
+
+#ifdef __CUDNN__
+    cudnnTensorDescriptor_t m_aInputTensorDesc, m_aOutputTensorDesc, m_aDeltaDesc, m_aInputDeltaDesc;
+    cudnnPoolingDescriptor_t m_aPoolingDesc;
+    DTYPE *m_pDevInput, *m_pDevOutput, *m_pDevInputDelta, *m_pDevDelta;
+    // DTYPE *m_aHostInput, *m_aHostOutput, *m_aHostInputDelta, *m_aHostDelta;
+
+    float m_alpha;
+    float m_beta;
+    double m_coef;
+#endif  // __CUDNN__
+
+public:
+    AvaragePooling2D(Operator<DTYPE> *pInput, int strideRow, int strideCol, int maskRow, int maskCol, std::string pName) : Operator<DTYPE>(pInput, pName) {
+        #ifdef __DEBUG__
+        std::cout << "AvaragePooling2D::AvaragePooling2D(Operator<DTYPE> *, int, int)" << '\n';
+        #endif  // __DEBUG__
+        this->Alloc(pInput, strideRow, strideCol, maskRow, maskCol);
+    }
+
+    AvaragePooling2D(Operator<DTYPE> *pInput, int strideRow, int strideCol, int maskRow, int maskCol, int padding, std::string pName) : Operator<DTYPE>(pInput, pName) {
+        #ifdef __DEBUG__
+        std::cout << "AvaragePooling2D::AvaragePooling2D(Operator<DTYPE> *, int, int, std::string)" << '\n';
+        #endif  // __DEBUG__
+        this->Alloc(pInput, strideRow, strideCol, maskRow, maskCol, padding, padding);
+    }
+
+    ~AvaragePooling2D() {
+        #ifdef __DEBUG__
+        std::cout << "AvaragePooling2D::~AvaragePooling2D()" << '\n';
+        #endif  // __DEBUG__
+#ifdef __CUDNN__
+        Delete();
+#endif  // if __CUDNN__
+    }
+
+    int Alloc(Operator<DTYPE> *pInput, int strideRow, int strideCol, int maskRow, int maskCol, int padding1 = 0, int padding2 = 0) {
+        #ifdef __DEBUG__
+        std::cout << "AvaragePooling2D::Alloc(Operator<DTYPE> *, int, int)" << '\n';
+        #endif  // __DEBUG__
+
+        Shape *shapeOfInput = pInput->GetResult()->GetShape();
+
+        m_stride[0] = strideRow;
+        m_stride[1] = strideCol;
+
+        m_mask[0] = maskRow;
+        m_mask[1] = maskCol;
+
+        m_padding[0] = padding1;
+        m_padding[1] = padding2;
+
+        int rowsize = 0;
+        int colsize = 0;
+
+        rowsize = ((*shapeOfInput)[3] - maskRow + (2 * m_padding[0])) / strideRow + 1;
+        colsize = ((*shapeOfInput)[4] - maskCol + (2 * m_padding[1])) / strideCol + 1;
+
+        this->SetResult(new Tensor<DTYPE>((*shapeOfInput)[0], (*shapeOfInput)[1], (*shapeOfInput)[2], rowsize, colsize));
+        this->SetDelta(new Tensor<DTYPE>((*shapeOfInput)[0], (*shapeOfInput)[1], (*shapeOfInput)[2], rowsize, colsize));
+
+        return TRUE;
+    }
+
+#ifdef __CUDNN__
+    void InitializeAttributeForGPU(unsigned int idOfDevice) {
+        Tensor<DTYPE> *input = this->GetInput()[0]->GetResult();
+        Shape *shapeOfInput  = input->GetShape();
+
+        Tensor<DTYPE> *result = this->GetResult();
+        Shape *shapeOfResult  = result->GetShape();
+
+        int batchsize   = (*shapeOfResult)[1];
+        int channelsize = (*shapeOfResult)[2];  // == shapeOfWeight[1]
+        int rowsize     = (*shapeOfResult)[3];
+        int colsize     = (*shapeOfResult)[4];
+
+        m_alpha = 1.f;
+        m_beta  = 0.f;
+
+        checkCUDNN(cudnnCreatePoolingDescriptor(&m_aPoolingDesc));
+
+        checkCUDNN(cudnnSetPooling2dDescriptor(m_aPoolingDesc, CUDNN_POOLING_AVERAGE_COUNT_EXCLUDE_PADDING, CUDNN_PROPAGATE_NAN,
+                                               m_mask[0], m_mask[1], m_padding[0], m_padding[1], m_stride[0], m_stride[1]));
+
+        checkCUDNN(cudnnGetPooling2dForwardOutputDim(m_aPoolingDesc, m_aInputTensorDesc,
+                                                     &batchsize, &channelsize, &rowsize, &colsize));
+    }
+
+#endif  // if __CUDNN__
+
+    void Delete() {
+#ifdef __CUDNN__
+
+        if (m_aPoolingDesc) checkCUDNN(cudnnDestroyPoolingDescriptor(m_aPoolingDesc));
+        m_aPoolingDesc = NULL;
+
+        checkCudaErrors(cudaThreadSynchronize());
+
+#endif  // if __CUDNN__
+    }
+
+    // 메모리 효율을 생각하면 time에 따라 취해야 할 액션이 다르다.
+    int ForwardPropagate(int pTime = 0) {
+        std::cout << "not yet cpu avg pooling" << std::endl;
+
+        return TRUE;
+    }
+
+    int BackPropagate(int pTime = 0) {
+        std::cout << "not yet cpu avg pooling" << std::endl;
+
+        return TRUE;
+    }
+
+#ifdef __CUDNN__
+    int ForwardPropagateOnGPU(int pTime = 0) {
+        Tensor<DTYPE> *input  = this->GetInput()[0]->GetResult();
+        Tensor<DTYPE> *result = this->GetResult();
+
+        m_pDevInput         = input->GetGPUData(pTime);
+        m_aInputTensorDesc  = input->GetDescriptor();
+        m_pDevOutput        = result->GetGPUData(pTime);
+        m_aOutputTensorDesc = result->GetDescriptor();
+
+        checkCUDNN(cudnnPoolingForward(this->GetCudnnHandle(), m_aPoolingDesc, &m_alpha, m_aInputTensorDesc, m_pDevInput,
+                                       &m_beta, m_aOutputTensorDesc, m_pDevOutput));
+
+
+        checkCudaErrors(cudaDeviceSynchronize());
+        return TRUE;
+    }
+
+    int BackPropagateOnGPU(int pTime = 0) {
+        Tensor<DTYPE> *input_delta = this->GetInput()[0]->GetDelta();
+        Tensor<DTYPE> *this_delta  = this->GetDelta();
+        Tensor<DTYPE> *input       = this->GetInput()[0]->GetResult();
+        Tensor<DTYPE> *result      = this->GetResult();
+
+        m_pDevInput         = input->GetGPUData(pTime);
+        m_aInputTensorDesc  = input->GetDescriptor();
+        m_pDevOutput        = result->GetGPUData(pTime);
+        m_aOutputTensorDesc = result->GetDescriptor();
+        m_pDevDelta         = this_delta->GetGPUData(pTime);
+        m_aDeltaDesc        = this_delta->GetDescriptor();
+        m_pDevInputDelta    = input_delta->GetGPUData(pTime);
+        m_aInputDeltaDesc   = input_delta->GetDescriptor();
+
+        checkCUDNN(cudnnPoolingBackward(this->GetCudnnHandle(), m_aPoolingDesc,
+                                        &m_alpha, m_aOutputTensorDesc, m_pDevOutput,
+                                        m_aDeltaDesc, m_pDevDelta, m_aInputTensorDesc, m_pDevInput,
+                                        &m_alpha, m_aInputDeltaDesc, m_pDevInputDelta));
+
+
+        checkCudaErrors(cudaDeviceSynchronize());
+        return TRUE;
+    }
 
 #endif  // if __CUDNN__
 };
--- tutorials/ImageNet/net/my_Densenet.hpp
+++ tutorials/ImageNet/net/my_Densenet.hpp
@@ -21,15 +21,42 @@ public:
         // 1
         out = new BatchNormalizeLayer<DTYPE>(out, TRUE, "BasicBlock_BN1" + pName);
         out = new Relu<DTYPE>(out, "BasicBlock_Relu1" + pName);
-        out = new ConvolutionLayer2D<DTYPE>(out, pNumInputChannel, 4*pGrowthRate, 1, 1, 1, 1, 0, FALSE, "BasicBlock_Conv1" + pName);
+        out = new ConvolutionLayer2D<DTYPE>(out, pNumInputChannel, 4 * pGrowthRate, 1, 1, 1, 1, 0, FALSE, "BasicBlock_Conv1" + pName);
 
         // 2
         out = new BatchNormalizeLayer<DTYPE>(out, TRUE, "BasicBlock_BN2" + pName);
         out = new Relu<DTYPE>(out, "BasicBlock_Relu2" + pName);
-        out = new ConvolutionLayer2D<DTYPE>(out, 4*pGrowthRate, pGrowthRate, 3, 3, 1, 1, 1, FALSE, "BasicBlock_Conv2" + pName);
+        out = new ConvolutionLayer2D<DTYPE>(out, 4 * pGrowthRate, pGrowthRate, 3, 3, 1, 1, 1, FALSE, "BasicBlock_Conv2" + pName);
 
         // Concat
-        out = new Addall<DTYPE>(remember, out, "ResNet_Skip_Add" + pName);
+        out = new ConcatenateChannelWise<float>(remember, out, "BasicBlock_ConCat");
+
+        this->AnalyzeGraph(out);
+
+        return TRUE;
+    }
+};
+
+template<typename DTYPE> class Transition : public Module<DTYPE>{
+private:
+public:
+    Transition(Operator<DTYPE> *pInput, int pNumInputChannel, int pNumOutputChannel, std::string pName = NULL) {
+        Alloc(pInput, pNumInputChannel, pNumOutputChannel, pName);
+    }
+
+    virtual ~Transition() {}
+
+    int Alloc(Operator<DTYPE> *pInput, int pNumInputChannel, int pNumOutputChannel, std::string pName) {
+        this->SetInput(pInput);
+
+        Operator<DTYPE> *out = pInput;
+
+        // 1
+        out = new BatchNormalizeLayer<DTYPE>(out, TRUE, "BasicBlock_BN1" + pName);
+        out = new Relu<DTYPE>(out, "BasicBlock_Relu1" + pName);
+        out = new ConvolutionLayer2D<DTYPE>(out, pNumInputChannel, pNumOutputChannel, 1, 1, 1, 1, 0, FALSE, "BasicBlock_Conv1" + pName);
+
+        // Avg Pooling
 
         this->AnalyzeGraph(out);
 
@@ -39,97 +66,97 @@ public:
 
 // template<typename DTYPE> class ResNet : public NeuralNetwork<DTYPE>{
 // private:
-//     int m_numInputChannel;
+// int m_numInputChannel;
 //
 // public:
-//     ResNet(Tensorholder<DTYPE> *pInput, Tensorholder<DTYPE> *pLabel, std::string pBlockType, int pNumOfBlock1, int pNumOfBlock2, int pNumOfBlock3, int pNumOfBlock4, int pNumOfClass) {
-//         Alloc(pInput, pLabel, pBlockType, pNumOfBlock1, pNumOfBlock2, pNumOfBlock3, pNumOfBlock4, pNumOfClass);
-//     }
+// ResNet(Tensorholder<DTYPE> *pInput, Tensorholder<DTYPE> *pLabel, std::string pBlockType, int pNumOfBlock1, int pNumOfBlock2, int pNumOfBlock3, int pNumOfBlock4, int pNumOfClass) {
+// Alloc(pInput, pLabel, pBlockType, pNumOfBlock1, pNumOfBlock2, pNumOfBlock3, pNumOfBlock4, pNumOfClass);
+// }
 //
-//     virtual ~ResNet() {}
+// virtual ~ResNet() {}
 //
-//     int Alloc(Tensorholder<DTYPE> *pInput, Tensorholder<DTYPE> *pLabel, std::string pBlockType, int pNumOfBlock1, int pNumOfBlock2, int pNumOfBlock3, int pNumOfBlock4, int pNumOfClass) {
-//         this->SetInput(2, pInput, pLabel);
+// int Alloc(Tensorholder<DTYPE> *pInput, Tensorholder<DTYPE> *pLabel, std::string pBlockType, int pNumOfBlock1, int pNumOfBlock2, int pNumOfBlock3, int pNumOfBlock4, int pNumOfClass) {
+// this->SetInput(2, pInput, pLabel);
 //
-//         m_numInputChannel = 64;
+// m_numInputChannel = 64;
 //
-//         Operator<DTYPE> *out = pInput;
+// Operator<DTYPE> *out = pInput;
 //
-//         // ReShape
-//         out = new ReShape<DTYPE>(out, 3, 224, 224, "ReShape");
-//         // out = new BatchNormalizeLayer<DTYPE>(out, TRUE, "BN0");
+//// ReShape
+// out = new ReShape<DTYPE>(out, 3, 224, 224, "ReShape");
+//// out = new BatchNormalizeLayer<DTYPE>(out, TRUE, "BN0");
 //
-//         // 1
-//         out = new ConvolutionLayer2D<DTYPE>(out, 3, m_numInputChannel, 7, 7, 2, 2, 3, FALSE, "Conv");
-//         out = new BatchNormalizeLayer<DTYPE>(out, TRUE, "BN0");
-//         out = new Relu<DTYPE>(out, "Relu0");
+//// 1
+// out = new ConvolutionLayer2D<DTYPE>(out, 3, m_numInputChannel, 7, 7, 2, 2, 3, FALSE, "Conv");
+// out = new BatchNormalizeLayer<DTYPE>(out, TRUE, "BN0");
+// out = new Relu<DTYPE>(out, "Relu0");
 //
-//         out = new Maxpooling2D<float>(out, 2, 2, 3, 3, 1, "MaxPool_2");
-//         // out = new BatchNormalizeLayer<DTYPE>(out, TRUE, "BN1");
+// out = new Maxpooling2D<float>(out, 2, 2, 3, 3, 1, "MaxPool_2");
+//// out = new BatchNormalizeLayer<DTYPE>(out, TRUE, "BN1");
 //
-//         out = this->MakeLayer(out, m_numInputChannel, pBlockType, pNumOfBlock1, 1, "Block1");
-//         out = this->MakeLayer(out, 128, pBlockType, pNumOfBlock2, 2, "Block2");
-//         out = this->MakeLayer(out, 256, pBlockType, pNumOfBlock3, 2, "Block3");
-//         out = this->MakeLayer(out, 512, pBlockType, pNumOfBlock3, 2, "Block4");
+// out = this->MakeLayer(out, m_numInputChannel, pBlockType, pNumOfBlock1, 1, "Block1");
+// out = this->MakeLayer(out, 128, pBlockType, pNumOfBlock2, 2, "Block2");
+// out = this->MakeLayer(out, 256, pBlockType, pNumOfBlock3, 2, "Block3");
+// out = this->MakeLayer(out, 512, pBlockType, pNumOfBlock3, 2, "Block4");
 //
-//         out = new BatchNormalizeLayer<DTYPE>(out, TRUE, "BN1");
-//         out = new Relu<DTYPE>(out, "Relu1");
+// out = new BatchNormalizeLayer<DTYPE>(out, TRUE, "BN1");
+// out = new Relu<DTYPE>(out, "Relu1");
 //
-//         out = new GlobalAvaragePooling2D<DTYPE>(out, "Avg Pooling");
+// out = new GlobalAvaragePooling2D<DTYPE>(out, "Avg Pooling");
 //
-//         out = new ReShape<DTYPE>(out, 1, 1, 512, "ReShape");
+// out = new ReShape<DTYPE>(out, 1, 1, 512, "ReShape");
 //
-//         out = new Linear<DTYPE>(out, 512, pNumOfClass, TRUE, "Classification");
-//         out = new BatchNormalizeLayer<DTYPE>(out, FALSE, "BN0");
+// out = new Linear<DTYPE>(out, 512, pNumOfClass, TRUE, "Classification");
+// out = new BatchNormalizeLayer<DTYPE>(out, FALSE, "BN0");
 //
-//         this->AnalyzeGraph(out);
+// this->AnalyzeGraph(out);
 //
-//         // ======================= Select LossFunction Function ===================
-//         this->SetLossFunction(new SoftmaxCrossEntropy<float>(out, pLabel, "SCE"));
-//         // SetLossFunction(new MSE<float>(out, label, "MSE"));
+//// ======================= Select LossFunction Function ===================
+// this->SetLossFunction(new SoftmaxCrossEntropy<float>(out, pLabel, "SCE"));
+//// SetLossFunction(new MSE<float>(out, label, "MSE"));
 //
-//         // ======================= Select Optimizer ===================
-//         // this->SetOptimizer(new GradientDescentOptimizer<float>(this->GetParameter(), 0.000001, 0.9, 5e-4, MINIMIZE));
-//         // this->SetOptimizer(new GradientDescentOptimizer<float>(this->GetParameter(), 0.001, MINIMIZE));
-//         this->SetOptimizer(new AdamOptimizer<float>(this->GetParameter(), 0.001, 0.9, 0.999, 1e-08, 5e-4, MINIMIZE));
+//// ======================= Select Optimizer ===================
+//// this->SetOptimizer(new GradientDescentOptimizer<float>(this->GetParameter(), 0.000001, 0.9, 5e-4, MINIMIZE));
+//// this->SetOptimizer(new GradientDescentOptimizer<float>(this->GetParameter(), 0.001, MINIMIZE));
+// this->SetOptimizer(new AdamOptimizer<float>(this->GetParameter(), 0.001, 0.9, 0.999, 1e-08, 5e-4, MINIMIZE));
 //
-//         return TRUE;
-//     }
+// return TRUE;
+// }
 //
-//     Operator<DTYPE>* MakeLayer(Operator<DTYPE> *pInput, int pNumOfChannel, std::string pBlockType, int pNumOfBlock, int pStride, std::string pName = NULL) {
-//         if (pNumOfBlock == 0) {
-//             return pInput;
-//         } else if ((pBlockType == "BasicBlock") && (pNumOfBlock > 0)) {
-//             Operator<DTYPE> *out = pInput;
+// Operator<DTYPE>* MakeLayer(Operator<DTYPE> *pInput, int pNumOfChannel, std::string pBlockType, int pNumOfBlock, int pStride, std::string pName = NULL) {
+// if (pNumOfBlock == 0) {
+// return pInput;
+// } else if ((pBlockType == "BasicBlock") && (pNumOfBlock > 0)) {
+// Operator<DTYPE> *out = pInput;
 //
-//             // Test of effect of the Max pool
-//             // if (pStride > 1) {
-//             // out = new Maxpooling2D<float>(out, pStride, pStride, 2, 2, "MaxPool_2");
-//             // }
+//// Test of effect of the Max pool
+//// if (pStride > 1) {
+//// out = new Maxpooling2D<float>(out, pStride, pStride, 2, 2, "MaxPool_2");
+//// }
 //
-//             out = new BasicBlock<DTYPE>(out, m_numInputChannel, pNumOfChannel, pStride, pName);
+// out = new BasicBlock<DTYPE>(out, m_numInputChannel, pNumOfChannel, pStride, pName);
 //
-//             int pNumOutputChannel = pNumOfChannel;
+// int pNumOutputChannel = pNumOfChannel;
 //
-//             // int pNumOutputChannel = pNumOfChannel;
-//             //
-//             // out =new BasicBlock<DTYPE>(out, m_numInputChannel, pNumOutputChannel, pStride, pName);
+//// int pNumOutputChannel = pNumOfChannel;
+////
+//// out =new BasicBlock<DTYPE>(out, m_numInputChannel, pNumOutputChannel, pStride, pName);
 //
-//             for (int i = 1; i < pNumOfBlock; i++) {
-//                 out = new BasicBlock<DTYPE>(out, pNumOutputChannel, pNumOutputChannel, 1, pName);
-//             }
+// for (int i = 1; i < pNumOfBlock; i++) {
+// out = new BasicBlock<DTYPE>(out, pNumOutputChannel, pNumOutputChannel, 1, pName);
+// }
 //
-//             m_numInputChannel = pNumOutputChannel;
+// m_numInputChannel = pNumOutputChannel;
 //
-//             return out;
-//         } else if ((pBlockType == "Bottleneck") && (pNumOfBlock > 0)) {
-//             return NULL;
-//         } else return NULL;
-//     }
+// return out;
+// } else if ((pBlockType == "Bottleneck") && (pNumOfBlock > 0)) {
+// return NULL;
+// } else return NULL;
+// }
 // };
 //
 // template<typename DTYPE> NeuralNetwork<DTYPE>* Resnet18(Tensorholder<DTYPE> *pInput, Tensorholder<DTYPE> *pLabel, int pNumOfClass) {
-//     return new ResNet<DTYPE>(pInput, pLabel, "BasicBlock", 2, 2, 2, 2, pNumOfClass);
+// return new ResNet<DTYPE>(pInput, pLabel, "BasicBlock", 2, 2, 2, 2, pNumOfClass);
 // }
 
 #endif  // ifndef DENSENET_H_
