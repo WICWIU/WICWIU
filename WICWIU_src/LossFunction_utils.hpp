9fe906bb52a9442a33a96743315da237653f2769
yoondonghwee <yoondonghwee@github.com>
2019-04-30 18:49:04 +0900

    First Init

--- WICWIU_src/LossFunction_utils.hpp
+++ WICWIU_src/LossFunction_utils.hpp
@@ -2,3 +2,6 @@
 #include "LossFunction/CrossEntropy.hpp"
 #include "LossFunction/MSE.hpp"
 #include "LossFunction/HingeLoss.hpp"
+
+#include "LossFunction/VanillaGeneratorLoss.hpp"
+#include "LossFunction/VanillaDiscriminatorLoss.hpp"
