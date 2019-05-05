be1575bfa13156e33d04374c153b0f699ccc5174
chuniee0126 <chunie0126@gmail.com>
2019-05-05 16:50:42 +0900

    Update DataLoader

--- WICWIU_src/DataLoader.hpp
+++ WICWIU_src/DataLoader.hpp
@@ -16,10 +16,10 @@
 #include "Dataset.hpp"
 
 /*!
-@class DataLoader 데이터를 로드하는 클래스
-@details [상세설명1 : 마지막에 작성예정]
-@details [상세설명2 : 마지막에 작성예정]
-*/
+ * @class DataLoader 데이터를 로드하는 클래스
+ * @details [상세설명1 : 마지막에 작성예정]
+ * @details [상세설명2 : 마지막에 작성예정]
+ */
 template<typename DTYPE> class DataLoader {
 private:
     /* data */
@@ -62,23 +62,24 @@ private:
     void Delete();
 
     void Init();
+    void ElicitInfo();
 
 public:
     DataLoader(Dataset<DTYPE> *dataset, int batchSize = 1, int useShuffle = FALSE, int numOfWorker = 1, int dropLast = TRUE);
     virtual ~DataLoader();
 
 
-    void              StartProcess();
-    void              StopProcess();
+    void                          StartProcess();
+    void                          StopProcess();
 
     // distribute data idx to each thread
-    void              DistributeIdxOfData2Thread();
+    void                          DistributeIdxOfData2Thread();
 
-    void              DataPreprocess();
+    void                          DataPreprocess();
 
-    void              Push2IdxBuffer(std::vector<int> *setOfIdx);
+    void                          Push2IdxBuffer(std::vector<int> *setOfIdx);
 
-    std::vector<int>* GetIdxSetFromIdxBuffer();
+    std::vector<int>            * GetIdxSetFromIdxBuffer();
 
     Tensor<DTYPE>               * Concatenate(std::queue<Tensor<DTYPE> *>& setOfData);
 
@@ -86,26 +87,26 @@ public:
 
     std::vector<Tensor<DTYPE> *>* GetDataFromGlobalBuffer();
 
-    //static int random_generator(int upperbound);
+    // static int random_generator(int upperbound);
 };
 
 
 /*!
-@brief 랜덤한 숫자를 생성하는 메서드
-@details 파라미터로 받은 값의 범위 내에서 랜덤한 숫자를 반환해준다.
-@param upperbound rand_max값
-@return (static한)랜덤한 숫자
-*/
+ * @brief 랜덤한 숫자를 생성하는 메서드
+ * @details 파라미터로 받은 값의 범위 내에서 랜덤한 숫자를 반환해준다.
+ * @param upperbound rand_max값
+ * @return (static한)랜덤한 숫자
+ */
 static int random_generator(int upperbound) {
-    srand (time(NULL));
+    srand(time(NULL));
     return (upperbound == 0) ? 0 : rand() % upperbound;
 }
 
 /*!
-@brief DataLoader 클래스를 동적 할당하는 메소드
-@details 사용할 thread갯수만큼을 메모리에 동적으로 할당한다.
-@return 없음
-*/
+ * @brief DataLoader 클래스를 동적 할당하는 메소드
+ * @details 사용할 thread갯수만큼을 메모리에 동적으로 할당한다.
+ * @return 없음
+ */
 template<typename DTYPE> void DataLoader<DTYPE>::Alloc() {
 #ifdef __DEBUG___
     std::cout << __FUNCTION__ << '\n';
@@ -117,10 +118,10 @@ template<typename DTYPE> void DataLoader<DTYPE>::Alloc() {
 }
 
 /*!
-@brief thread들이 사용한 공간을 반환하는 메소드
-@details DataLoader클래스 생성때 할당받았던 thread공간을 반납한다.
-@return 없음
-*/
+ * @brief thread들이 사용한 공간을 반환하는 메소드
+ * @details DataLoader클래스 생성때 할당받았던 thread공간을 반납한다.
+ * @return 없음
+ */
 template<typename DTYPE> void DataLoader<DTYPE>::Delete() {
 #ifdef __DEBUG___
     std::cout << __FUNCTION__ << '\n';
@@ -163,10 +164,10 @@ template<typename DTYPE> void DataLoader<DTYPE>::Delete() {
 }
 
 /*!
-@brief DataLoader클래스의 각 멤버 변수를 초기화하는 메소드
-@details DataLoader클래스의 각 멤버 변수가 가지고 있던 값들을 초기화한다.
-@return 없음
-*/
+ * @brief DataLoader클래스의 각 멤버 변수를 초기화하는 메소드
+ * @details DataLoader클래스의 각 멤버 변수가 가지고 있던 값들을 초기화한다.
+ * @return 없음
+ */
 template<typename DTYPE> void DataLoader<DTYPE>::Init() {
 #ifdef __DEBUG___
     std::cout << __FUNCTION__ << '\n';
@@ -184,18 +185,35 @@ template<typename DTYPE> void DataLoader<DTYPE>::Init() {
     m_useShuffle             = FALSE; //
 }
 
+/*!
+ * @brief
+ * @details
+ * @return
+ */
+template<typename DTYPE> void DataLoader<DTYPE>::ElicitInfo() {
+#ifdef __DEBUG___
+    std::cout << __FUNCTION__ << '\n';
+    std::cout << __FILE__ << '\n';
+#endif  // ifdef __DEBUG___
+    // need to free memory which was allocated at Alloc()
+    // elicit information of data;
+    m_lenOfDataset = m_pDataset->GetLength();
+    assert(m_lenOfDataset > 0);
+    m_numOfEachDatasetMember = m_pDataset->GetNumOfDatasetMember();
+    assert(m_numOfEachDatasetMember > 0);
+}
 
 /*!
-@brief DataLoader의 생성자
-@details 데이터, batchSize, useshuffle, thread갯수, dropLast를 파라미터로 받아
-@details DataLoader클래스를 생성한다.
-@param *dataset 데이터셋
-@param batchSize 데이터를 몇개씩 사용할지를 담은 변수
-@param useShuffle shuffle할지를 결정하는 변수
-@param numOfWorker thread갯수
-@param dropLast batch단위보다 작은 데이터 처리방법
-@return 없음
-*/
+ * @brief DataLoader의 생성자
+ * @details 데이터, batchSize, useshuffle, thread갯수, dropLast를 파라미터로 받아
+ * @details DataLoader클래스를 생성한다.
+ * @param *dataset 데이터셋
+ * @param batchSize 데이터를 몇개씩 사용할지를 담은 변수
+ * @param useShuffle shuffle할지를 결정하는 변수
+ * @param numOfWorker thread갯수
+ * @param dropLast batch단위보다 작은 데이터 처리방법
+ * @return 없음
+ */
 template<typename DTYPE> DataLoader<DTYPE>::DataLoader(Dataset<DTYPE> *dataset, int batchSize, int useShuffle, int numOfWorker, int dropLast) {
 #ifdef __DEBUG___
     std::cout << __FUNCTION__ << '\n';
@@ -216,11 +234,7 @@ template<typename DTYPE> DataLoader<DTYPE>::DataLoader(Dataset<DTYPE> *dataset, 
     // Drop last
     m_dropLast = dropLast;
 
-    // elicit information of data;
-    m_lenOfDataset = m_pDataset->GetLength();
-    assert(m_lenOfDataset > 0);
-    m_numOfEachDatasetMember = m_pDataset->GetNumOfDatasetMember();
-    assert(m_numOfEachDatasetMember > 0);
+    this->ElicitInfo();
 
 #ifdef __DEBUG___
     std::cout << m_pDataset << '\n';
@@ -244,12 +258,11 @@ template<typename DTYPE> DataLoader<DTYPE>::DataLoader(Dataset<DTYPE> *dataset, 
     this->StartProcess();
 }
 
-
 /*!
-@brief DataLoader 클래스 소멸자
-@details StopProcess 메소드를 호출해서 thread처리를 멈추고 클래스를 소멸시킨다.
-@return 없음
-*/
+ * @brief DataLoader 클래스 소멸자
+ * @details StopProcess 메소드를 호출해서 thread처리를 멈추고 클래스를 소멸시킨다.
+ * @return 없음
+ */
 template<typename DTYPE> DataLoader<DTYPE>::~DataLoader() {
 #ifdef __DEBUG___
     std::cout << __FUNCTION__ << '\n';
@@ -260,13 +273,12 @@ template<typename DTYPE> DataLoader<DTYPE>::~DataLoader() {
     this->Delete();
 }
 
-
 /*!
-@brief thread를 만들고 DataPreprocess메소드를 불러서 thread를 돌리는 메소드
-@details m_nowWorking을 TRUE로 바꿔주고 thread를 만든다.
-@details 그리고 실제 thread를 돌리는 메소드인 DataPreprocess를 호출한다.
-@return 없음
-*/
+ * @brief thread를 만들고 DataPreprocess메소드를 불러서 thread를 돌리는 메소드
+ * @details m_nowWorking을 TRUE로 바꿔주고 thread를 만든다.
+ * @details 그리고 실제 thread를 돌리는 메소드인 DataPreprocess를 호출한다.
+ * @return 없음
+ */
 template<typename DTYPE> void DataLoader<DTYPE>::StartProcess() {
     // Generate thread for Dist - DistributeIdxOfData2Thread()
     // Generate thread set for Process -
@@ -285,16 +297,16 @@ template<typename DTYPE> void DataLoader<DTYPE>::StartProcess() {
     }
 }
 
-
 /*!
-@brief [어떤 메소드인지 타이틀]
-@details m_nowWorking을 FALSE로 바꿔주고
-@return 없음
-*/
+ * @brief [어떤 메소드인지 타이틀]
+ * @details m_nowWorking을 FALSE로 바꿔주고
+ * @return 없음
+ */
 template<typename DTYPE> void DataLoader<DTYPE>::StopProcess() {
     // Stop Thread
     // not deallocate!
     m_nowWorking = FALSE;
+
     for (int i = 0; i < m_numOfWorker; i++) {
         sem_post(&m_globalEmpty);  // for thread terminate
     }
@@ -309,60 +321,56 @@ template<typename DTYPE> void DataLoader<DTYPE>::StopProcess() {
     printf("Join dataloader base thread\r\n");
 }
 
-
 /*!
-@brief [어떤 메소드인지 타이틀]
-@details [메소드 상세설명]
-@param [파라미터 변수명1] [상세설명]
-@param [파라미터 변수명2] [상세설명]
-@return 없음
-*/
+ * @brief [어떤 메소드인지 타이틀]
+ * @details [메소드 상세설명]
+ * @param [파라미터 변수명1] [상세설명]
+ * @param [파라미터 변수명2] [상세설명]
+ * @return 없음
+ */
 template<typename DTYPE> void DataLoader<DTYPE>::DistributeIdxOfData2Thread() {
     std::vector<int> *allOfIdx = new std::vector<int>(m_lenOfDataset);
     std::vector<int> *setOfIdx = NULL;
-    int dropLastSize = m_lenOfDataset % m_batchSize; //num of dropLast
-    int numOfBatchBlockSize = m_lenOfDataset / m_batchSize;
-    int cnt = 0;
+    int dropLastSize           = m_lenOfDataset % m_batchSize; // num of dropLast
+    int numOfBatchBlockSize    = m_lenOfDataset / m_batchSize;
+    int cnt                    = 0;
 
-    for(int i=0; i < m_lenOfDataset; i++) (*allOfIdx)[i] = i;
+    for (int i = 0; i < m_lenOfDataset; i++) (*allOfIdx)[i] = i;
 
     if (m_useShuffle) std::random_shuffle(allOfIdx->begin(), allOfIdx->end(), random_generator);
 
     while (m_nowWorking) {
-
         setOfIdx = new std::vector<int>(m_batchSize);
 
         for (int i = 0; i < m_batchSize; i++) {
             (*setOfIdx)[i] = (*allOfIdx)[m_batchSize * cnt + i];
             #ifdef __DEBUG__
-             std::cout << "idx:" << m_batchSize * cnt + i << " " <<(*setOfIdx)[i] << " " ;
-            #endif //__DEBUG__
+            std::cout << "idx:" << m_batchSize * cnt + i << " " << (*setOfIdx)[i] << " ";
+            #endif  // __DEBUG__
         }
         cnt++;
 
         this->Push2IdxBuffer(setOfIdx);
 
-        if(numOfBatchBlockSize == cnt){
-            if(!m_dropLast && dropLastSize){
-            std::reverse(allOfIdx->begin(), allOfIdx->end());
-                if(m_useShuffle)
-                    std::random_shuffle(allOfIdx->begin() + dropLastSize, allOfIdx->end(), random_generator);
+        if (numOfBatchBlockSize == cnt) {
+            if (!m_dropLast && dropLastSize) {
+                std::reverse(allOfIdx->begin(), allOfIdx->end());
+
+                if (m_useShuffle) std::random_shuffle(allOfIdx->begin() + dropLastSize, allOfIdx->end(), random_generator);
             } else {
-                if(m_useShuffle)
-                    std::random_shuffle(allOfIdx->begin(), allOfIdx->end(), random_generator);
+                if (m_useShuffle) std::random_shuffle(allOfIdx->begin(), allOfIdx->end(), random_generator);
             }
             cnt = 0;
         }
     }
 }
 
-
 /*!
-@brief thread를 돌리는 메소드
-@details 임시 localBuffer를 만들고 이 곳에 인덱스 버퍼로 부터 데이터를 받는다.
-@details 그 다음 preprocessedData에 localBuffer의 데이터 셋을 합쳐서 글로벌 버퍼로 넘긴다.
-@return 없음
-*/
+ * @brief thread를 돌리는 메소드
+ * @details 임시 localBuffer를 만들고 이 곳에 인덱스 버퍼로 부터 데이터를 받는다.
+ * @details 그 다음 preprocessedData에 localBuffer의 데이터 셋을 합쳐서 글로벌 버퍼로 넘긴다.
+ * @return 없음
+ */
 template<typename DTYPE> void DataLoader<DTYPE>::DataPreprocess() {
     // for thread
     // doing all of thing befor push global buffer
@@ -414,7 +422,6 @@ template<typename DTYPE> void DataLoader<DTYPE>::DataPreprocess() {
     delete[] localBuffer;
 }
 
-
 template<typename DTYPE> void DataLoader<DTYPE>::Push2IdxBuffer(std::vector<int> *setOfIdx) {
     sem_wait(&m_distIdxEmpty);
     sem_wait(&m_distIdxMutex);
@@ -448,7 +455,7 @@ template<typename DTYPE> Tensor<DTYPE> *DataLoader<DTYPE>::Concatenate(std::queu
     temp     = setOfData.front();
     capacity = temp->GetCapacity();
     // Shape를 이용해서 Shape대로 만들어질 수 있도록 수정(Shape constructor 잘돌아가는지 확인)
-    result   = Tensor<DTYPE>::Zeros(1, m_batchSize, 1, 1, capacity);
+    result = Tensor<DTYPE>::Zeros(1, m_batchSize, 1, 1, capacity);
 
     // std::cout << result->GetShape() << '\n';
     // std::cout << setOfData.size() << '\n';
@@ -485,10 +492,10 @@ template<typename DTYPE> void DataLoader<DTYPE>::Push2GlobalBuffer(std::vector<T
 }
 
 /*!
-@brief 글로벌 버퍼에 있는 데이터를 가져오는 메소드
-@details
-@return [상세설명]
-*/
+ * @brief 글로벌 버퍼에 있는 데이터를 가져오는 메소드
+ * @details
+ * @return [상세설명]
+ */
 template<typename DTYPE> std::vector<Tensor<DTYPE> *> *DataLoader<DTYPE>::GetDataFromGlobalBuffer() {
     sem_wait(&m_globalFull);
     sem_wait(&m_globalMutex);
@@ -502,6 +509,5 @@ template<typename DTYPE> std::vector<Tensor<DTYPE> *> *DataLoader<DTYPE>::GetDat
 
     return preprocessedData;
 }
-
 
 #endif  // ifndef DATALOADER_H_
