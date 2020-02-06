d3ec381d40e8a0901313b77deb05069d3e1c5fe7
JinMyeongSuh <21500343@handong.edu>
2020-02-07 02:46:22 +0900

    add RNN/main.cpp

--- Test/RecurrentOperatorTest.cpp
+++ Test/RecurrentOperatorTest.cpp
@@ -4,89 +4,59 @@
 //class Recurrent;
 
 int main(int argc, char const *argv[]) {
+    clock_t startTime = 0, endTime = 0;
+    double  nProcessExcuteTime = 0;
 
-    int time_size = 1;
-    int input_size = 5;
-    int hidden_size = 10;
-    int output_size = 5;
+    char alphabet[] = "helo";
+    int outDim = 4;
 
-    //Tensor에 UseTime 잘 확인하기!!!
-    //처음부터 원하는 값으로 초기화하는 방법은 없나?
-    Tensorholder<float> *input0 = new Tensorholder<float>(Tensor<float>::Random_normal(time_size, 1, 1, 1, input_size, 0.0, 0.1), "RecurrentLayer_pWeight_h2o_");
-    Tensorholder<float> *pWeight_x2h = new Tensorholder<float>(Tensor<float>::Random_normal(time_size, 1, 1, hidden_size, input_size, 0.0, 0.1), "RecurrentLayer_pWeight_x2h_");
-    Tensorholder<float> *pWeight_h2h = new Tensorholder<float>(Tensor<float>::Random_normal(time_size, 1, 1, hidden_size, hidden_size, 0.0, 0.1), "RecurrentLayer_pWeight_h2h_");
-    Tensorholder<float> *pWeight_h2o = new Tensorholder<float>(Tensor<float>::Random_normal(time_size, 1, 1, output_size, hidden_size, 0.0, 0.1), "RecurrentLayer_pWeight_h2o_");
+    char inputStr[] = "hell";
+    char desiredStr[] = "ello";
 
-    //input값 설정
-    for(int i = 0; i < time_size*input_size; i++){
-      (*(input0->GetResult()))[i] = 1;                                    // 연산자 오버로딩은 Tensor에 있느듯
+    char seqLen = 4;
+	int inputSeq[] = { 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0 };
+	int desiredSeq[] = { 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1 };
+
+    Tensor<float> *x_tensor = new Tensor<float>(4, 1, 1, 1, 4);
+    Tensorholder<float> *x_holder = new Tensorholder<float>(x_tensor, "x");
+
+    Tensor<float> *label_tensor = new Tensor<float>(4, 1, 1, 1, 4);
+    Tensorholder<float> *label_holder = new Tensorholder<float>(label_tensor, "label");
+
+    //RNN<float> *net = new my_RNN<float>(x_holder,label_holder);
+
+    for(int t = 0; t < seqLen; t++){
+        for (int ba = 0; ba < 1; ba++) {
+            for(int col = 0; col < 4; col++){
+                (*x_tensor)[Index5D(x_tensor->GetShape(), t, ba, 0, 0, col)] = inputSeq[Index5D(x_tensor->GetShape(), t, ba, 0, 0, col)];
+                printf("Index5D-x_tensor: %d\n", Index5D(x_tensor->GetShape(), t, ba, 0, 0, col));
+                (*label_tensor)[Index5D(label_tensor->GetShape(), t, ba, 0, 0, col)] = desiredSeq[Index5D(x_tensor->GetShape(), t, ba, 0, 0, col)];
+                printf("Index5D-label_tensor: %d\n", Index5D(x_tensor->GetShape(), t, ba, 0, 0, col));
+            }
+        }
     }
 
-    //x2h weight 값 설정
-    for(int i = 0; i < time_size*input_size * hidden_size; i++){
-      (*(pWeight_x2h->GetResult()))[i] = 2;
-    }
+    printf("Getshape \n");
+    std::cout << x_holder->GetResult()->GetShape() << '\n';
+    printf("GetResult \n");
+    std::cout << x_holder->GetResult() << '\n';
 
-    //h2h weight 값 설정
-    for(int i = 0; i < time_size*hidden_size * hidden_size; i++){
-      (*(pWeight_h2h->GetResult()))[i] = 2;
-    }
+    printf("Getshape \n");
+    std::cout << label_holder->GetResult()->GetShape() << '\n';
+    printf("GetResult \n");
+    std::cout << label_holder->GetResult() << '\n';
 
-    //h2o weight 값 설정
-    for(int i = 0; i < time_size*hidden_size * output_size; i++){
-      (*(pWeight_h2o->GetResult()))[i] = 2;
-    }
+    endTime            = clock();
+    nProcessExcuteTime = ((double)(endTime - startTime)) / CLOCKS_PER_SEC;
+    printf("\n(excution time per epoch : %f)\n\n", nProcessExcuteTime);
 
-    std::cout << pWeight_x2h->GetResult()->GetShape() << '\n';
-    std::cout << pWeight_x2h->GetResult() << '\n';
+    //std::cout <<
 
-    Operator<float> *rnn = new Recurrent<float>(input0, pWeight_x2h, pWeight_h2h, pWeight_h2o, "RNN");
+    delete x_tensor;
+    delete x_holder;
+    delete label_tensor;
+    delete label_holder;
 
-    std::cout << '\n';
+    return 0;
+}
 
-    #ifdef __CUDNN__
-      cudnnHandle_t m_cudnnHandle;
-      cudnnCreate(&m_cudnnHandle);
-      pWeight->SetDeviceGPU(m_cudnnHandle, 0);
-      input0->SetDeviceGPU(m_cudnnHandle, 0);
-      matmul->SetDeviceGPU(m_cudnnHandle, 0);
-    #endif  // ifdef __CUDNN__
-
-    #ifdef __CUDNN__
-          rnn->ForwardPropagateOnGPU();
-    #else // ifdef __CUDNN__
-          rnn->ForwardPropagate();
-    #endif  // ifdef __CUDNN__
-
-
-    std::cout << "*****************ForwardPropagate 후****************" << '\n';
-
-
-
-    std::cout << rnn->GetResult()->GetShape() << '\n';
-    std::cout << rnn->GetResult() << '\n';
-
-
-    for(int i = 0; i < time_size * output_size; i++){
-      (*(rnn->GetDelta()))[i] = i;
-    }
-
-    std::cout << "*****************BackPropagate 후****************" << '\n';
-
-    #ifdef __CUDNN__
-          rnn->BackPropagateOnGPU();
-    #else // ifdef __CUDNN__
-          rnn->BackPropagate();
-    #endif  // ifdef __CUDNN__
-
-    std::cout << rnn->GetDelta()->GetShape() << '\n';
-    std::cout << rnn->GetDelta() << '\n';
-
-    delete input0;
-    delete pWeight_x2h;
-    delete pWeight_h2h;
-    delete pWeight_h2o;
-
-
-
-    }
