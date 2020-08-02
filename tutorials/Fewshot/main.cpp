fb87efee0ff279909ce865261d06143372c1a8be
ccsss <chansol0505@naver.com>
2020-08-02 21:08:32 +0900

    fewshot: Update tutorials/Fewshot/main.cpp [ getDirList() : read directory ]

--- tutorials/Fewshot/main.cpp
+++ tutorials/Fewshot/main.cpp
@@ -1,3 +1,34 @@
-int main(int argc, char const *argv[]) {
-    return 0;
+#include <string>
+#include <iostream>
+#include <dirent.h>
+#include <sys/types.h>
+
+DIR* getDirList(char * dirName)
+{
+    DIR *dir;
+    try
+    {
+        if ((dir = opendir(dirName)) == NULL)
+            throw "could not open directory";
+    }
+    catch (const char* msg)
+    {
+        std::cerr << msg << std::endl;
+        return nullptr;
+    }
+    return dir;
+}
+
+
+int main(int argc, char const *argv[])
+{
+    DIR* dir = getDirList("/tmp/casia_train");
+    struct dirent *ent;
+
+    while ((ent = readdir(dir)) != NULL)
+    {
+        std::cout << ent->d_name << std::endl;
+    }
+    closedir(dir);
+    return EXIT_SUCCESS;
 }
