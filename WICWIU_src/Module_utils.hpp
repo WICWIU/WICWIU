68d8c233b257af3ba3d0bf74e8b9b50940e9c3e0
ChanhyoLee <21700589@handong.edu>
2021-12-03 17:49:53 +0900

    add new modules

--- WICWIU_src/Module_utils.hpp
+++ WICWIU_src/Module_utils.hpp
@@ -10,9 +10,30 @@
 #include "Module/RecurrentLayer.hpp"
 #include "Module/LSTMLayer.hpp"
 #include "Module/GRULayer.hpp"
+#include "Module/GRUCellLayer.hpp"
+#include "Module/BidirectionalGRULayer.hpp"
 
 #include "Module/EmbeddingLayer.hpp"
 
 #include "Module/Encoder.hpp"
 #include "Module/Decoder.hpp"
+
+#include "Module/RNNEncoder.hpp"
+#include "Module/RNNDecoder.hpp"
+
+#include "Module/AttentionWeight.hpp"
+#include "Module/Attention.hpp"
+#include "Module/LayerNormalizeLayer.hpp"
+#include "Module/TransformerFeedForwardLayer.hpp"
+#include "Module/TransformerSubLayerConnection.hpp"
+#include "Module/TransformerEncoderLayer.hpp"
+#include "Module/TransformerDecoderLayer.hpp"
+#include "Module/TransformerPELayer.hpp"
+#include "Module/TransformerGenerator.hpp"
+#include "Module/TransformerEncoder.hpp"
+#include "Module/TransformerDecoder.hpp"
+#include "Module/TransformerEncoderModule.hpp"
+#include "Module/TransformerDecoderModule.hpp"
+
+#include "Module/Bahdanau.hpp"
 #endif // ifndef __MODULE_UTIL_H__
